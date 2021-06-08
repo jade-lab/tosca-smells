9b8a574b2d52464b9818cc67cc5c04a4b96d5d96
stefanodallapalma <stefano.dallapalma0@gmail.com>
2021-06-08 15:28:58 +0200

    Updated clustering and data

--- clustering.py
+++ clustering.py
@@ -1,134 +1,159 @@
 import os
-import numpy as np
-import pandas as pd
-
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
 
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import RobustScaler
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
+metrics = pd.read_csv(os.path.join('data', 'metrics.csv')).fillna(0)
 
-metrics = pd.read_csv(os.path.join('data', 'metrics.csv'))
+# 1. Remove duplicates
+metrics.drop_duplicates(subset=metrics.columns.difference(['url']), inplace=True)
 
-X = metrics.drop(['url'], axis=1).fillna(0).drop_duplicates()
+X = metrics.drop(['url'], axis=1)
 
-# Normalize
+# 2. Remove correlated variables (i.e., features for which VIF > 10)
+while True:
+    vif_data = pd.DataFrame()
+    vif_data["feature"] = X.columns
+    vif_data["VIF"] = [variance_inflation_factor(X.values, i) for i in range(len(X.columns))]
+
+    if any([vif > 10 for vif in vif_data.VIF]):
+        # Remove feature with the highest VIF
+        idx = vif_data[['VIF']].idxmax().iloc[0]
+        print(f'Removing {vif_data.iloc[idx].feature} (VIF > 10)')
+        X.drop(vif_data.iloc[idx].feature, axis=1, inplace=True)
+    else:
+        break
+
+print(f'\nThe following columns are retained: {" ".join(X.columns.to_list())}')
+
+# 3. Normalize data
 columns = X.columns
-X = pd.DataFrame(StandardScaler().fit_transform(X), columns=columns)
+scaler = RobustScaler()
+X = pd.DataFrame(scaler.fit_transform(X), columns=columns)
 
-best_config = {
-    'clusterer': None,
-    'cluster_labels': None,
-    'n_clusters': 0,
-    'silhouette_avg': -1
-}
+# 4. Clustering
+N_CLUSTERS = 2
+kmeans = KMeans(n_clusters=N_CLUSTERS, random_state=42)
+cluster_labels = kmeans.fit_predict(X)
 
-# Identify best number of clusters based on silhouette score
-range_n_clusters = [2, 3, 5, 8, 13, 21, 34, 65]
-for n_clusters in range_n_clusters:
+# The silhouette_score gives the average value for all the samples.
+# This gives a perspective into the density and separation of the formed clusters
+silhouette_avg = silhouette_score(X, cluster_labels)
+print("\nFor n_clusters =", N_CLUSTERS,
+      "The average silhouette_score is :", silhouette_avg)
 
-    clusterer = KMeans(n_clusters=n_clusters, random_state=42)
-    cluster_labels = clusterer.fit_predict(X)
-
-    # The silhouette_score gives the average value for all the samples.
-    # This gives a perspective into the density and separation of the formed clusters
-    silhouette_avg = silhouette_score(X, cluster_labels)
-    print("For n_clusters =", n_clusters,
-          "The average silhouette_score is :", silhouette_avg)
-
-    if silhouette_avg > best_config['silhouette_avg']:
-        best_config['clusterer'] = clusterer
-        best_config['cluster_labels'] = cluster_labels
-        best_config['n_clusters'] = n_clusters
-        best_config['silhouette_avg'] = silhouette_avg
-
-print("The number of clusters that maximize the mean silhouette coefficient is", best_config['n_clusters'])
-
+# ==================== UNCOMMENT SECTION BELOW AND CHANGE feature1 AND feature2 TO PLOT CLUSTERS =======================
 # Create a subplot with 1 row and 2 columns
-fig, ax = plt.subplots()
-fig.set_size_inches(18, 10)
+# fig, ax = plt.subplots()
+# fig.set_size_inches(18, 10)
+#
+# # Plot showing the actual clusters formed
+# colors = cm.nipy_spectral(cluster_labels.astype(float) / N_CLUSTERS)
+#
+# feature1 = 'lines_code'
+# feature2 = 'num_interfaces'
+# ax.scatter(X[feature1], X[feature2], marker='.', s=70, lw=0, alpha=1, c=colors, edgecolor='k')
+#
+# # Labeling the clusters
+# centers = kmeans.cluster_centers_
+#
+# # Draw white circles at cluster centers
+# ax.scatter(centers[:, 0], centers[:, 1], marker='o', c="white", alpha=1, s=200, edgecolor='k')
+#
+# for i, c in enumerate(centers):
+#     ax.scatter(c[0], c[1], marker='$%d$' % i, alpha=1, s=25, edgecolor='k')
+#
+# ax.set_title("The visualization of the clustered data.")
+# ax.set_xlabel(f'Feature space for {feature1}')
+# ax.set_ylabel(f'Feature space for {feature2}')
+#
+# plt.suptitle(("Silhouette analysis for KMeans clustering on sample data "
+#               "with n_clusters = %d" % N_CLUSTERS),
+#              fontsize=14, fontweight='bold')
+#
+# plt.show()
+# ===================================================== END TMP =====================================================
 
-# Plot showing the actual clusters formed
-colors = cm.nipy_spectral(best_config['cluster_labels'].astype(float) / best_config['n_clusters'])
 
-feature1 = 'lines_code'
-feature2 = 'num_node_types'
-ax.scatter(X[feature1], X[feature2], marker='.', s=70, lw=0, alpha=1, c=colors, edgecolor='k')
-
-# Labeling the clusters
-centers = best_config['clusterer'].cluster_centers_
-
-# Draw white circles at cluster centers
-ax.scatter(centers[:, 0], centers[:, 1], marker='o', c="white", alpha=1, s=200, edgecolor='k')
-
-for i, c in enumerate(centers):
-    ax.scatter(c[0], c[1], marker='$%d$' % i, alpha=1, s=25, edgecolor='k')
-
-ax.set_title("The visualization of the clustered data.")
-ax.set_xlabel(f'Feature space for {feature1}')
-ax.set_ylabel(f'Feature space for {feature2}')
-
-plt.suptitle(("Silhouette analysis for KMeans clustering on sample data "
-              "with n_clusters = %d" % best_config['n_clusters']),
-             fontsize=14, fontweight='bold')
-
-plt.show()
-
-X['url'] = metrics.url
+# 5. Label clusters
+X['url'] = metrics.url.to_list()
 clusters = {}
 
 # Create an empty DataFrame for each cluster
-for idx in set(best_config['cluster_labels']):
+for idx in set(cluster_labels):
     clusters[idx] = pd.DataFrame()
 
-for i in range(len(best_config['cluster_labels'])):
-    cluster_id = best_config['cluster_labels'][i]
+for i in range(len(cluster_labels)):
+    cluster_id = cluster_labels[i]
     clusters[cluster_id] = clusters[cluster_id].append(X.iloc[i])
 
-asfm = []  # Average Sum of Feature values of each Module
+# Compute the Sum of Feature values of each Module for each cluster
+# and store its average
+asfm = {}
+for id, cluster in clusters.items():
+    asfm[id] = np.mean(cluster.sum(axis=1))
 
-# Compute the SFM for each cluster
-for _, cluster in clusters.items():
-    cluster["sfm"] = cluster.sum(axis=1)
-    asfm.append(cluster["sfm"].mean())
-
-# Compute the Mean of SFM
-masfm = np.mean(asfm)
+id_highest_asfm = max(asfm, key=asfm.get)
 
 for id, cluster in clusters.items():
-    cluster["cluster_id"] = id
-    cluster["smelly"] = True if cluster["sfm"].mean() >= masfm else False
-    print(cluster["smelly"].to_list().count(True), 'smelly instances in cluster', id)
+    urls = cluster.url.to_list()
+    cluster.drop(['url'], axis=1, inplace=True)
 
-merged_clusters = pd.concat(cluster for cluster in clusters.values())[['cluster_id', 'url', 'sfm', 'smelly']]
+    # Transforming back to original metric value
+    columns = cluster.columns
+    clusters[id] = pd.DataFrame(scaler.inverse_transform(cluster), columns=columns)
+
+    clusters[id]["cluster_id"] = id
+    clusters[id]["url"] = urls
+
+    if id == id_highest_asfm:
+        clusters[id]["smelly"] = True
+    else:
+        clusters[id]["smelly"] = False
+
+# Merge and save cluster data
+merged_clusters = pd.concat(cluster for cluster in clusters.values())[['cluster_id', 'url', 'smelly']]
 merged_clusters.to_csv(os.path.join('data', 'clusters.csv'), index=False)
 
-# Mann-Whitney U test satistical tests between smelly and sound clusters
-import scipy.stats as stats
+
+# 5. Statistical Analysis
+def cohen_d(d1, d2):
+    # calculate the size of samples
+    n1, n2 = len(d1), len(d2)
+    # calculate the variance of the samples
+    s1, s2 = np.var(d1, ddof=1), np.var(d2, ddof=1)
+    # calculate the pooled standard deviation
+    s = np.sqrt(((n1 - 1) * s1 + (n2 - 1) * s2) / (n1 + n2 - 2))
+    # calculate the means of the samples
+    u1, u2 = np.mean(d1), np.mean(d2)
+    # calculate the effect size
+    return (u1 - u2) / s
+
 
 smelly_cluster = pd.concat(cluster for cluster in clusters.values() if cluster.smelly.to_list().count(True) > 0)
 sound_cluster = pd.concat(cluster for cluster in clusters.values() if cluster.smelly.to_list().count(False) > 0)
 
-print('Performing the Mann–Whitney U test to compare the distribution of a given feature within the set of smelly '
-      'blueprints against the set of sound blueprints. The null hypothesis states that the feature is distributed '
-      'independently from whether the blueprint is smelly or not. If rejected, it means that the feature is distributed'
-      ' differently in each set and thus is a promising candidate as input to detect the smell.')
+smelly_cluster.drop(['url', 'cluster_id', 'smelly'], axis=1,  inplace=True)
+n_tests = len(smelly_cluster.columns)
 
-n_tests = len(smelly_cluster.columns) - 4
+print('\nResults from Mann-Whitney U test')
+
 for feature in smelly_cluster.columns:
-    if feature in ('url', 'sfm', 'cluster_id', 'smelly'):
-        continue
-
     group1 = smelly_cluster[feature]
     group2 = sound_cluster[feature]
 
     u_stat, p_value = stats.mannwhitneyu(group1, group2, alternative='greater')
+    d = cohen_d(group1, group2)
 
-    print(f'The probability that \'{feature}\' is distributed independently from whether the blueprint is smelly or not'
-          f' is: {p_value*n_tests} (U={u_stat})')
+    if p_value * n_tests < 0.01:
+        print(f'\'{feature}\' is distributed independently from whether the blueprint is smelly or not '
+              f'(corrected p={p_value * n_tests}, U={u_stat}, d={d} smelly: {np.mean(group1)}, sound {np.mean(group2)})')
 
-
-# We used the Bonferroni correction to correct for multiple testing for the 17 features we tested. Therefore, we test
-# against the stricter significance level of 0.00059, which corresponds to a non-corrected p ≤ 0.01 for each individual test.
+print(f'\nNumber of tests: {n_tests}, corrected p-value: {0.01 / n_tests}')
