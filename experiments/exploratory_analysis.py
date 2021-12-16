fbc70f367a818b424fe8388cf5701a69725eb09b
stefanodallapalma <stefano.dallapalma0@gmail.com>
2021-12-16 17:27:42 +0100

    Added script for exploratory analysis of metrics distribution

---
+++ experiments/exploratory_analysis.py
@@ -0,0 +1,73 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+from cliffs_delta import cliffs_delta
+
+
+class ExploratoryAnalysis:
+
+    def run(self):
+
+        metrics_df = pd.read_csv(os.path.join('data', 'metrics.csv')).fillna(0)
+        metrics_df['num_types_and_templates'] = metrics_df[['num_node_templates', 'num_relationship_templates',
+                                                            'num_node_types', 'num_relationship_types']].sum(axis=1)
+        metrics_df = metrics_df[
+            ['url', 'lines_code', 'num_types_and_templates', 'lcot', 'num_interfaces', 'num_properties']]
+
+        validation_df = pd.read_csv(os.path.join('data', 'validation.csv'))
+        group_all = metrics_df.merge(validation_df[['url', 'final']], on='url')
+        group_smelly = group_all[group_all.final == True].drop(['final'], axis=1)
+        group_sound = group_all[group_all.final == False].drop(['final'], axis=1)
+
+        results = pd.DataFrame()
+        n_tests = group_smelly.columns.size
+
+        for feature in group_smelly.columns:
+
+            u_stat, p_value = stats.mannwhitneyu(group_smelly[feature], group_sound[feature], alternative='greater')
+            d, res = cliffs_delta(group_smelly[feature], group_sound[feature])
+
+            if p_value * n_tests < 0.01:
+                results = results.append({
+                    'feature': feature.upper(),
+                    'p-value': p_value,
+                    'p-value corrected': p_value * n_tests,
+                    'U': u_stat,
+                    'cliff_delta': d,
+                    'mean smelly': round(group_smelly[feature].mean()),
+                    'mean sound': round(group_sound[feature].mean())
+                }, ignore_index=True)
+
+        ncols = 3
+        nrows = round((group_all.shape[1] - 2) / 3)
+        fig, axs = plt.subplots(nrows=nrows, ncols=ncols)
+
+        i = 0
+        j = 0
+
+        for metric in group_all.columns:
+            if metric in ('smelly', 'url', 'final'):
+                continue
+
+            # smelly = group_all.query('final == True')[metric]
+            # sound = group_all.query('final == False')[metric]
+
+            # Combine data
+            # plot_data = list([smelly, sound])
+            plot_data = list([group_smelly[metric], group_sound[metric]])
+
+            xticklabels = ['Blob', 'Sound']
+
+            axs[i, j % ncols].set_xlabel(metric.replace('_', ' ').upper())
+            axs[i, j % ncols].set_xticks([1, 2])
+            axs[i, j % ncols].set_xticklabels(xticklabels)
+            axs[i, j % ncols].violinplot(plot_data, showmedians=True, showextrema=False)
+            j += 1
+
+            if j % ncols == 0:
+                i += 1
+
+        plt.show()
+
+        return results
