9e56aa95e5ed474e7803844b69fc2d5ad87e4751
stefanodallapalma <stefano.dallapalma0@gmail.com>
2022-01-12 02:19:15 +0100

    updated label size

--- experiments/exploratory_analysis.py
+++ experiments/exploratory_analysis.py
@@ -6,18 +6,20 @@ from cliffs_delta import cliffs_delta
 
 
 def main():
+    plt.rc('ytick', labelsize=12)
+    plt.rc('xtick', labelsize=12)
+    plt.rc('axes', labelsize=14)
 
     metrics_df = pd.read_csv(os.path.join('data', 'metrics.csv')).fillna(0)
     metrics_df['num_types_and_templates'] = metrics_df[['num_node_templates', 'num_relationship_templates',
                                                         'num_node_types', 'num_relationship_types']].sum(axis=1)
-    metrics_df = metrics_df[
-        ['url', 'lines_code', 'num_types_and_templates', 'lcot', 'num_interfaces', 'num_properties', 'num_imports',
-         'num_capabilities', 'num_inputs', 'num_suspicious_comments']]
+    metrics_df = metrics_df[['url', 'lines_code', 'lcot', 'num_types_and_templates', 'num_interfaces', 'num_properties',
+                             'num_imports']]
 
     validation_df = pd.read_csv(os.path.join('data', 'validation.csv'))
     group_all = metrics_df.merge(validation_df[['url', 'final']], on='url')
-    group_smelly = group_all[group_all.final == True].drop(['final'], axis=1)
-    group_sound = group_all[group_all.final == False].drop(['final'], axis=1)
+    group_smelly = group_all[group_all.final == True].drop(['url', 'final'], axis=1)
+    group_sound = group_all[group_all.final == False].drop(['url', 'final'], axis=1)
 
     results = pd.DataFrame()
     n_tests = group_smelly.columns.size
@@ -27,16 +29,16 @@ def main():
         u_stat, p_value = stats.mannwhitneyu(group_smelly[feature], group_sound[feature], alternative='greater')
         d, res = cliffs_delta(group_smelly[feature], group_sound[feature])
 
-        if p_value * n_tests < 0.01:
-            results = results.append({
-                'feature': feature.upper(),
-                'p-value': p_value,
-                'p-value corrected': p_value * n_tests,
-                'U': u_stat,
-                'cliff_delta': d,
-                'mean smelly': round(group_smelly[feature].mean()),
-                'mean sound': round(group_sound[feature].mean())
-            }, ignore_index=True)
+        results = results.append({
+            'feature': feature.upper(),
+            'p-value': p_value,
+            'p-value corrected': p_value * n_tests,
+            'U': u_stat,
+            'cliff_delta': d,
+            'mean smelly': round(group_smelly[feature].mean()),
+            'mean sound': round(group_sound[feature].mean()),
+            'significant': '**yes**' if p_value * n_tests < 0.01 else 'no'
+        }, ignore_index=True)
 
     ncols = 3
     nrows = round((group_all.shape[1] - 2) / 3)
